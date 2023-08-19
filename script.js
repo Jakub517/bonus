f5a03cae111b7f2ca04ce607de949e0e637ca95d
Jakub517 <pavlu.jakub8@seznam.cz>
2023-08-19 10:39:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const codeInput = document.getElementById("codeInput");
+const generateButton = document.getElementById("generateButton");
+const bonusDiv = document.getElementById("bonus");
+
+generateButton.addEventListener("click", () => {
+    const code = codeInput.value;
+    var sifrovano = "U2FsdGVkX19n7v/H85napb8QPdx3nnyWFqW1zHQgfyAxYl5DHYR13wLp9sfwFGGx"
+    var desifrovano = CryptoJS.AES.decrypt(sifrovano, code);
+    try {
+        var vysledek = desifrovano.toString(CryptoJS.enc.Utf8);
+        if (vysledek == ""){
+            throw new Error;
+        } 
+
+        const bonusContent = `
+        <h2>Gratulujeme!</h2>
+        <p>Až budeš u Bonusu nezapomeň si vygenerovat Diplom!</p>
+        <p>Kód je: <b>${code}</b></p>
+        <p>Datum zobrazení je: <b>${new Date().toLocaleDateString()}</b></p>
+        <p>Vaše souřadnice jsou:</p>
+        <p>${vysledek}</p>
+        `;
+
+        bonusDiv.innerHTML = bonusContent;
+    } catch (error) {
+        bonusDiv.innerHTML = "<p>Neplatný kód. Zkus to znovu. <br> <b> Možná příčina: </b> Dodržujte velká a malá písmena.</p>";
+    }
+});
